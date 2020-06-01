16de42f2b39a9c97417449367a317e253f87c007
Melwin <mel.langfeld@gmx.de>
2020-06-01 18:21:15 +0200

    Icon Update

--- app/src/main/java/com/advicy/lactapp/viewModels/SettingsViewModel.java
+++ app/src/main/java/com/advicy/lactapp/viewModels/SettingsViewModel.java
@@ -1,7 +1,23 @@
 package com.advicy.lactapp.viewModels;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 public class SettingsViewModel extends ViewModel {
     // TODO: Implement the viewModel
+    //profile, sound, notifications
+    private MutableLiveData<String> profile;
+
+    public MutableLiveData<String> getProfile() {
+        if (profile == null) {
+            profile = new MutableLiveData<String>();
+        }
+        return profile;
+    }
+
+
 }
